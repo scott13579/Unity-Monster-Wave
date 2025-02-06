f11a6b6a385419c9f4f8cf5927dfa04bd8675edb
scott13579 <scott135@naver.com>
2025-02-06 20:01:47 +0900

    외부 리소스 추가 및 SpawnManager 구현 중

---
+++ Assets/02.Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    /// [변수]
+    /// 생성하려는 프리팹
+    /// 여러 개를 담고 있는 배열로 생성
+    public GameObject[] spawnPrefabs;
+    
+    /// 생성하는 주기
+    public float maxTime = 3f;
+    private float timer = 0f;
+
+    /// [기능]
+    /// 몬스터 스폰 기능
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= maxTime)
+        {
+            timer = 0f;
+            // 몬스터 생성
+            Instantiate(spawnPrefabs[0], transform.position, Quaternion.identity);
+        }
+    }
+}
